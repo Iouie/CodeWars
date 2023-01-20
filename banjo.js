b5da0984bd9c70eae73e78e6ce3d63a7317e59f6
Louie <sxl7953@rit.edu>
2023-01-19 23:23:46 -0500

    fixed a problem

--- banjo.js
+++ banjo.js
@@ -5,8 +5,10 @@ If your name starts with the letter "R" or lower case "r", you are playing banjo
 
 //Answer//
   // Implement me
-  if(name.charAt(0) === 'r' || name.charAt(0) === 'R'){
-  return name + " plays banjo";
-    }
-  else return name + ' does not play banjo';
-}
+  function areYouPlayingBanjo(name) {
+    // Implement me
+    if(name.charAt(0) === 'r' || name.charAt(0) === 'R'){
+    return name + " plays banjo";
+      }
+    else return name + ' does not play banjo';
+  }
