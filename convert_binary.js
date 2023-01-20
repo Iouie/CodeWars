574826174e7d8ac238efb39988b06e6229b4be48
Louie <31904100+Iouie@users.noreply.github.com>
2023-01-20 00:58:13 -0500

    Create convert_binary.js

---
+++ convert_binary.js
@@ -0,0 +1,10 @@
+/* 
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+*/
+
+//Answer//
+
+function toBinary(n){
+  return Number(n.toString(2))
+}
