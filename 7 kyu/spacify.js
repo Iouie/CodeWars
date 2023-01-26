ea7a549aadda387fc42fff5f98db9978642a8298
Louie <31904100+Iouie@users.noreply.github.com>
2023-01-26 15:36:14 -0500

    Create spacify.js

---
+++ 7 kyu/spacify.js
@@ -0,0 +1,9 @@
+/*
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+*/
+
+//Answer//
+
+function spacify(str) {
+  return str.split('').join(' ')
+}
