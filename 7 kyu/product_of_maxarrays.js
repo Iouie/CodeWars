593335ed2d3ef2e9e9dad586d3846a9c567ab091
Louie <31904100+Iouie@users.noreply.github.com>
2023-02-02 12:17:11 -0500

    Create product_of_maxarrays.js

---
+++ 7 kyu/product_of_maxarrays.js
@@ -0,0 +1,21 @@
+/*
+Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+Notes
+Array/list size is at least 3 .
+
+Array/list's numbers Will be mixture of positives , negatives and zeros
+
+Repetition of numbers in the array/list could occur.
+*/
+
+//Answer//
+
+function maxProduct(numbers, size){
+  let prod = 1;
+  let sorted = numbers.sort((a,b)=> a-b)
+  for(let i = numbers.length - 1; i >= numbers.length - size; i--){
+    prod *= numbers[i]
+  }
+  return prod
+}
