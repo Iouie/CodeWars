1ffb89b18a531a161f73dc0798ad955126996387
Louie <31904100+Iouie@users.noreply.github.com>
2023-02-04 16:48:47 -0500

    Create arrow_functinos_odd.js

---
+++ arrow_functinos_odd.js
@@ -0,0 +1,10 @@
+/*
+Time to test your basic knowledge in functions! Return the odds from a list:
+*/
+
+//Answer//
+
+function odds(values){
+  // arrow it
+  return values.filter( x => x % 2 === 1 );
+}
