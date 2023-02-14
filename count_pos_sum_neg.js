24895f67540935ded13109a26f7c1776b41cb7b2
Louie <31904100+Iouie@users.noreply.github.com>
2023-02-13 21:39:27 -0500

    Create count_pos_sum_neg.js

---
+++ count_pos_sum_neg.js
@@ -0,0 +1,14 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+*/
+
+//Answer//
+
+function countPositivesSumNegatives(input) {
+return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
